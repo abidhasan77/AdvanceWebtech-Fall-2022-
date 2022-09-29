e3bfc59cc19752cde130d26379c20d2dd501d747
abidhasan77 <91404741+abidhasan77@users.noreply.github.com>
2022-09-29 21:02:38 +0600

    first commit

---
+++ APWT_Task_1/resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+    @extends('layouts.app')
+    @section('content')
+    <h2>About Us:</h2>
+    <p>Our Company is well known tech company. We do provide all kind services. We are commited to give best service to our customer <br>
+    
+    </p>
+    @endsection
+</body>
+</html>
